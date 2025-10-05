343c28eef1005765b7794327725f73c2b6b7e223
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-05 19:48:28 +0400

    Add pathlib

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,19 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+DATABASES = {}
+
+if os.getenv('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config()
+else:
+    DATABASES['default'] = {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
