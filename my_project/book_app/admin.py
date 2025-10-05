e8014529a2091e8e02d7388c4bdbbcfd391e5072
tamuna-tamo <102787453+TAMUNA-CHKOIDZE@users.noreply.github.com>
2025-10-05 21:20:35 +0400

    add styles

--- my_project/book_app/admin.py
+++ my_project/book_app/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from book_app.models import Book
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author']
+    list_display = ['title', 'author', 'photo_tag']
+
+    # ადმინში, რომ პატარა ფოტო გამოჩნდეს ატვირთვის შემდეგ
+    def photo_tag(self, obj):
+        if obj.photo:
+            return format_html('<img src="{}" style="width: 50px; height:auto;" />', obj.photo.url)
+        return "-"
+
+    photo_tag.short_description = 'Photo'
 
 
 admin.site.register(Book, BookAdmin)
--- my_project/book_app/models.py
+++ my_project/book_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=100, verbose_name='title')
     author = models.CharField(max_length=100, verbose_name='author')
+    photo = models.ImageField(upload_to='book_photos', verbose_name='photo')
 
     def __str__(self):
         return f"{self.title} by {self.author}"
